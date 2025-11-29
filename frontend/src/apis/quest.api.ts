05e58b7adb0ad99da3ceefcf6184dc1267f50f76
RATSAPORN <rodolf8680@gmail.com>
2025-11-30 03:34:14 +0700

    fix

--- frontend/src/apis/quest.api.ts
+++ frontend/src/apis/quest.api.ts
@@ -2,16 +2,22 @@ import ApiService from "../services/api.service";
 import { Quest } from "../types/quest.type";
 
 export async function generateQuestApi(userId: number): Promise<Quest> {
-  const response = await ApiService.post(`/quests/${userId}`);
+  const response = await ApiService.post(
+    `${process.env.BACKEND_URl}/quests/${userId}`
+  );
   return response.data.quest;
 }
 
 export async function getUserQuestsApi(userId: number): Promise<Quest[]> {
-  const response = await ApiService.get(`/quests/${userId}`);
+  const response = await ApiService.get(
+    `${process.env.BACKEND_URl}/quests/${userId}`
+  );
   return response.data.quests;
 }
 
 export async function completeQuestApi(questId: number): Promise<Quest> {
-  const response = await ApiService.post(`/quests/complete/${questId}`);
+  const response = await ApiService.post(
+    `${process.env.BACKEND_URl}/quests/complete/${questId}`
+  );
   return response.data.quest;
 }
